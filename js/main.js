a9abf14bf83083c78dc57364762b6011909b60fa
Kimberly Woodward <kimberly.mcbain@gmail.com>
2015-04-29 19:55:36 -0400

    fixed map

--- js/main.js
+++ js/main.js
@@ -6,35 +6,33 @@
    HTML structures and invoke the proper JS code accordingly. Be sure
    to reference the Bootstrap documentation.)
 */
-$('#myTab a').click(function (e) {
-  e.preventDefault();
-  $(this).tab('show');
-});
-$('#myTab a[href="#profile"]').tab('show'); // Select tab by name
-$('#myTab a:first').tab('show');// Select first tab
-$('#myTab a:last').tab('show'); // Select last tab
-$('#myTab li:eq(2) a').tab('show'); // Select third tab (0-indexed)
+
 // TODO: Inside of your on ready handler, invoke the Leaflet.js library
 // to draw a map in your `#map-container` div.
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map-container').setView([46.852, -121.760], 13);
 // TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
 // tiles served through the MapQuest CDN. Consult this example to set up
 // the map tiles layers:
 
-L.tileLayer('http://{s}.tiles.mapbox.com/v3/MapID/{z}/{x}/{y}.png', {
+/* L.tileLayer('http://{s}.tiles.mapbox.com/v3/kimmyrae.m2504n80/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18
-}).addTo(kimmyrae.m2504n80);
+    maxZoom: 18 */
+/* }).addTo(map); */
+L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+}).addTo(map);
+ 
 // TODO: Customize that Map to show markers with popups at no fewer than 3
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
-var marker = L.marker([51.5, -0.09]).addTo(map);
-var circle = L.circle([51.508, -0.11], 500, {
+var marker = L.marker([46.852, -121.760]).addTo(map);
+var circle = L.circle([46.852, -121.760], 500, {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5
 }).addTo(map);
 var popup = L.popup()
-    .setLatLng([51.5, -0.09])
+    .setLatLng([46.852, -121.760])
     .setContent("I am a standalone popup.")
     .openOn(map);
