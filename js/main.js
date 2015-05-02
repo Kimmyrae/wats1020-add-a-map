72daf8b59b89797e2bb2595040187c0c78e7eba5
Kimberly Woodward <kimberly.mcbain@gmail.com>
2015-05-02 15:12:24 -0400

    added points of interest on a map

--- js/main.js
+++ js/main.js
@@ -27,19 +27,28 @@ L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
 var marker = L.marker([46.852, -121.760]).addTo(map);
-var circle = L.circle([46.852, -121.760], 500, {
+/*var circle = L.circle([46.852, -121.760], 500, {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5
-}).addTo(map);
+}).addTo(map);*/
+
 var popup = L.popup()
     .setLatLng([46.852, -121.760])
-    .setContent("Mount Rainier is right here!")
+    .setContent("Mount Rainier is right here! Elevation is 14,411 ft (4,392 m)")
     .openOn(map);
-var marker = L.marker([46.852, -121.760]).addTo(map);
-marker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
 
-var greenIcon = L.icon({
+var marker1 = L.marker([46.852, -121.760]).addTo(map);
+marker1.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
+	
+var marker2 = L.marker([46.8495529, -121.7123171]).addTo(map);
+	marker2.bindPopup("<b>Little Tahoma Peak</b><br>Elevation:11,138' (3,395 m), Mountain range-Cascade Range, First ascent:1894");
+	  
+	
+var marker3 = L.marker([46.851, -121.759]).addTo(map);
+	marker3.bindPopup("<b>Emmons Glacier</b><br>Emmons Glacier has the largest surface area of any glacier in the contiguous United States.");
+	
+/*var greenIcon = L.icon({
     iconUrl: 'leaf-green.png',
     shadowUrl: 'leaf-shadow.png',
 
@@ -48,5 +57,5 @@ var greenIcon = L.icon({
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
+});*/
 L.marker([46.852, -121.760], {icon: greenIcon}).addTo(map);
